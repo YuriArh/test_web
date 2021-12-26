402794c9615e1c4f2ebaf9b1efc122c9edce7290
Yuri <arhipow.yuri@gmail.com>
2021-12-27 03:09:35 +0600

    Finished setting up the project

---
+++ public/dist/test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var _require = require("underscore"),
+    object = _require.object; // средство  es6: блок "левых" объявлений
+
+
+var sentences = [{
+  subject: 'JavaScript',
+  verb: 'is',
+  object: 'great'
+}, {
+  subject: 'Elephants',
+  verb: 'are',
+  object: 'large'
+}]; // средство es6: деструктуризация объекта 
+
+function say(_ref) {
+  var subject = _ref.subject,
+      verb = _ref.verb,
+      object = _ref.object;
+  //средство es6: строки шаблона
+  console.log("".concat(subject, " ").concat(verb, " ").concat(object));
+}
+
+; //средство es6: for..of
+
+for (var _i = 0, _sentences = sentences; _i < _sentences.length; _i++) {
+  var s = _sentences[_i];
+  say(s);
+}
